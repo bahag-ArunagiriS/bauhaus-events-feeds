09c6d94c1a6acf894a3fa10b75f07ac8834b861b
bahag-ArunagiriS <112637696+bahag-ArunagiriS@users.noreply.github.com>
2022-11-04 13:20:51 +0100

    Revert "Feature add components"

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BAUHAUS Feed App';
+  title = 'frontend';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { EventFormComponent } from './events/event-form.component';
-import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent, EventFormComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
