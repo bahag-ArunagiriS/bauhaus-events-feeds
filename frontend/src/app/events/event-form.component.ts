6bc797bc4431c8747adfc9379262595d13a759fb
weilin-ong <ongweilin93@gmail.com>
2022-11-04 13:26:47 +0100

    Revert "Revert "Feature add components""
    This reverts commit 09c6d94c1a6acf894a3fa10b75f07ac8834b861b.

---
+++ frontend/src/app/events/event-form.component.ts
@@ -0,0 +1,51 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Event} from "../models/event";
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-event-form',
+  templateUrl: './event-form.component.html',
+  styleUrls: ['./event-form.component.css']
+})
+export class EventFormComponent implements OnInit {
+
+  eventForm!: FormGroup;
+  @Output() createEvent = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // add validation using forms - FormGroup
+    this.eventForm = new FormGroup({
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Z0-9 ]*$')
+      ]),
+      description: new FormControl('', Validators.required),
+      location: new FormControl('',Validators.required)
+    });
+  }
+  // add function
+  add(): void {
+    const form = this.eventForm.value;
+
+    //emit is used to fire an event
+    this.createEvent.emit(
+      new Event(form.title,'',form.description, form.location,0));
+    this.eventForm.reset();
+  }
+
+
+  //fetch title
+  get title() { return this.eventForm.get('title')!; }
+
+  //fetch description
+  get description() { return this.eventForm.get('description')!; }
+
+  //fetch location
+  get location() { return this.eventForm.get('location')!; }
+
+}
---
+++ frontend/src/app/events/events.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Event } from '../models/event';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventsService {
+  constructor(private httpClient: HttpClient) {}
+
+  getEvents(): Observable<Event[]> {
+    return this.httpClient.get<Event[]>
+    (`${environment.url}/events`);
+  }
+  addEvent(event: Event): Observable<Event> {
+    return this.httpClient.post<Event>
+    (`${environment.url}/event`, event)
+  }
+}
